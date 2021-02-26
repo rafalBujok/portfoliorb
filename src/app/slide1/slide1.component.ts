c10ff29a501dd183f2774be5ebd7c5d98830e40e
Rafał Bujok <rafal.bujok@gmail.com>
2021-02-26 11:42:17 +0100

    change font, rework slide1 component

--- src/app/slide1/slide1.component.ts
+++ src/app/slide1/slide1.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slide1',
   templateUrl: './slide1.component.html',
-  styleUrls: ['./slide1.component.css']
+  styleUrls: ['./slide1.component.scss']
 })
 export class Slide1Component implements OnInit {
 
